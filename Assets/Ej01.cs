1b530573ccd00639f860070b16a41cb91753559a
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-04-08 11:51:25 -0300

    Todo terminadito ❤😎

---
+++ Assets/Ej01.cs
@@ -0,0 +1,31 @@
+﻿//1.Crea un programa que permita ingresar por Inspector 
+//dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej01 : MonoBehaviour
+{
+
+    public int n1;
+    public int n2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (n1 == n2)
+        {
+            Debug.Log("Si son iguales");
+        }
+        else
+        {
+            Debug.Log("No son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej02.cs
@@ -0,0 +1,41 @@
+﻿//2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+//    Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej02 : MonoBehaviour
+{
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    public int dinero_disponible;
+    float suma;
+    float resto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        suma = (precio1 + precio2 + precio3);
+        resto = (dinero_disponible - suma);
+
+        if (suma > dinero_disponible)
+        {
+            Debug.Log("Suma supera dinero disponible ");
+            Debug.Log(resto);
+
+        }
+        else
+        {
+            Debug.Log("Suma es menos que el dinero disponible");
+            Debug.Log(resto);
+
+        }
+        Debug.Log("El resto es:  " + resto);
+    }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
